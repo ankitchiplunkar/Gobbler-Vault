47828ac5ab1c299c38c86447cad191b541b0e09a
Ankit Chiplunkar <ankitchiplunkar@gmail.com>
2022-09-30 10:21:49 +0100

    removed tasks from hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,9 +4,6 @@ import type { HardhatUserConfig } from "hardhat/config";
 import type { NetworkUserConfig } from "hardhat/types";
 import { resolve } from "path";
 
-import "./tasks/accounts";
-import "./tasks/deploy";
-
 const dotenvConfigPath: string = process.env.DOTENV_CONFIG_PATH || "./.env";
 dotenvConfig({ path: resolve(__dirname, dotenvConfigPath) });
 
